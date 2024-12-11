73985b3b2b6bda36b1a49921b5620aab33929389
Oscar Gomez Fuente <oscargomezf@gmail.com>
2024-12-11 18:47:43 +0100

    Añadir fichero de prueba hook

--- prueba_1.py
+++ prueba_1.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # /*
+#  * @autor Oscar Gomez Fuente <oscargomezf@gmail.com>
 #  * @file prueba_1.py
 #  * @author Oscar Gomez Fuente <oscargomezf@gmail.com>
 #  * @modified Oscar Gomez Fuente <oscargomezf@gmail.com>
-#  * @date 2024-12-11 18:44:12 
-#  * @version 47e0484
+#  * @date 2024-12-11 18:47:43 
+#  * @version
 #  * @section DESCRIPTION
 #  *     Script to test git_propset.py
 #  */
