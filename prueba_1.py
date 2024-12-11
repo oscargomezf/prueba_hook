0f29978d60502ab923393ed918813221abc6011a
Oscar Gomez Fuente <oscargomezf@gmail.com>
2024-12-11 18:30:01 +0100

    Añadir fichero de prueba hook

--- prueba_1.py
+++ prueba_1.py
@@ -2,12 +2,12 @@
 # /**
 #  * @file prueba_1.py
 #  * @author Oscar Gomez Fuente <oscargomezf@gmail.com>
-#  * @version e8abb63
-#  * @modified Oscar Gomez Fuente <oscargomezf@gmail.com>
-#  * @date 2024-12-11 18:28:17
+#  * @version 8388a23 e8abb63
+#  * @modified Oscar Gomez Fuente <oscargomezf@gmail.com> Oscar Gomez Fuente <oscargomezf@gmail.com>
+#  * @date 2024-12-11 18:28:33  2024-12-11 18:28:17
 #  * @section DESCRIPTION
 #  *     Script to test git_propset.py
 #  */
 
 if __name__ == "__main__":
-    print('Esta es otra prueba más y otra y otra para ver si funciona el script: git_propset.py')
+    print('Esta es otra prueba y otra más y otra y otra para ver si funciona el script: git_propset.py')
