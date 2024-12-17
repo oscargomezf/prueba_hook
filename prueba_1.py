a431a0d8a7a1b7fb301590575cf8cd5427693b4a
Oscar Gomez Fuente <oscargomezf@gmail.com>
2024-12-17 19:42:43 +0100

    Prueba ultima

--- prueba_1.py
+++ prueba_1.py
@@ -3,8 +3,8 @@
 #  * @autor Oscar Gomez Fuente <oscargomezf@gmail.com>
 #  * @file prueba_1.py
 #  * @modified Oscar Gomez Fuente <oscargomezf@gmail.com>
-#  * @date 2024-12-17 19:40:13 
-#  * @version 810cb58
+#  * @date 2024-12-17 19:42:16 
+#  * @version cd31585
 #  * @section DESCRIPTION
 #  *     Script to test git_propset.py
 #  */
