9613dc278a949bc6e2d4b9c772a5f72d5be87f77
Oscar Gomez Fuente <oscargomezf@gmail.com>
2024-12-17 19:28:05 +0100

    Prueba b3e0e3e9eb29f8b8e930abc22cb88fb381554ba8

--- prueba_1.py
+++ prueba_1.py
@@ -3,11 +3,13 @@
 #  * @autor Oscar Gomez Fuente <oscargomezf@gmail.com>
 #  * @file prueba_1.py
 #  * @modified Oscar Gomez Fuente <oscargomezf@gmail.com>
-#  * @date 2024-12-17 19:19:43 
-#  * @version 8de327a92b0321fefb67e4ff23a4e2ece1840ce5
+#  * @date 2024-12-17 19:25:56 
+#  * @version 8acf2490255974b5cdf91c1ba8af433e06ae4078
 #  * @section DESCRIPTION
 #  *     Script to test git_propset.py
 #  */
+
+
 
 
 
