63e9947d89e84a0bbcf28f166e65cde9f3831422
Oscar Gomez Fuente <oscargomezf@gmail.com>
2024-12-17 19:45:40 +0100

    Prueba ultima 1

--- prueba_1.py
+++ prueba_1.py
@@ -3,11 +3,12 @@
 #  * @autor Oscar Gomez Fuente <oscargomezf@gmail.com>
 #  * @file prueba_1.py
 #  * @modified Oscar Gomez Fuente <oscargomezf@gmail.com>
-#  * @date 2024-12-17 19:44:48 
-#  * @version 373f176
+#  * @date 2024-12-17 19:45:40 
+#  * @version 1e07a60
 #  * @section DESCRIPTION
 #  *     Script to test git_propset.py
 #  */
+
 
 if __name__ == "__main__":
     print('Esta es otra pfdsfds rueba y otraFRFR  más y otfrfrra ccy otra para ver si funciona el script: git_propset.py')
